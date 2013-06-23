1c940eae965a731b9ec2d619216542688cc99c96
Alexander Heusingfeld <aheusingfeld@firstpoint.de>
2013-06-23 12:34:38 +0200

    Fix #1: Introduce JVM warm-up phase

--- src/main/java/com/innoq/samples/concurrent/forkjoin/ForkJoinSummarizer.java
+++ src/main/java/com/innoq/samples/concurrent/forkjoin/ForkJoinSummarizer.java
@@ -13,72 +13,124 @@ import java.util.concurrent.RecursiveTask;
  */
 public class ForkJoinSummarizer extends RecursiveTask<Long> {
 
-    /**
-     * Threshold indicating when sequential work is more efficient.
-     */
-    private static int SEQ_THRESHOLD = 700;
-    private static int SUM_UP_TO = 5_000_000;
-
     private final List<Integer> num;
+    private final int seqThreshold;
 
-    public ForkJoinSummarizer(List<Integer> num) {
+    public ForkJoinSummarizer(List<Integer> num, final int seqThreshold) {
         this.num = num;
+        this.seqThreshold = seqThreshold;
     }
 
     @Override
     protected Long compute() {
-        if (num.size() < SEQ_THRESHOLD) {
+        if (num.size() < seqThreshold) {
             return sum(num);
         } else {
-            int midPosition = num.size() / 2;
-            final ForkJoinSummarizer sum1 = new ForkJoinSummarizer(num.subList(0, midPosition));
+            final int midPosition = num.size() / 2;
+            final ForkJoinSummarizer sum1 = new ForkJoinSummarizer(num.subList(0, midPosition), seqThreshold);
             sum1.fork();
-            final ForkJoinSummarizer sum2 = new ForkJoinSummarizer(num.subList(midPosition, num.size()));
+            final ForkJoinSummarizer sum2 = new ForkJoinSummarizer(num.subList(midPosition, num.size()), seqThreshold);
             return sum2.compute() + sum1.join();
         }
     }
 
-    private Long sum(List<Integer> num) {
+    private Long sum(final List<Integer> num) {
         long result = 0;
         for (long n : num) {
             // sum odd numbers
             result += (n % 2 == 0) ? 0 : n;
         }
+        // NOTE: This test neglects stripping the additional effort needed for auto-boxing from 'long' to 'Long'
         return result;
     }
 
 
-    // utility methods
 
-    public static void main(String[] args) {
+    // static utility methods
 
-        final List<Integer> list = initializeList(SUM_UP_TO);
-        Long result = 0l;
-
-        // do it sequentially
-        long start = System.currentTimeMillis();
-        for (int i : list) {
-            result += (i % 2 == 0) ? 0 : i;
+    private static Long sumOddEntries(List<Integer> num) {
+        long result = 0;
+        for (long n : num) {
+            // sumOddEntries odd numbers
+            result += (n % 2 == 0) ? 0 : n;
         }
-        System.out.println("Result (seq): " + result + " Duration: " + (System.currentTimeMillis() - start));
-
-
-        // (potentially) do it in parallel
-        final ForkJoinSummarizer summary = new ForkJoinSummarizer(list);
-
-        // actually 'Runtime.getRuntime().availableProcessors()' is used by default!
-        final ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
-        start = System.currentTimeMillis();
-        result = pool.invoke(summary);
-        System.out.println("Result (parallel): " + result + "; Duration: " + (System.currentTimeMillis() - start));
-        System.out.println("      Parallelism: " + pool.getParallelism() + "; ThreadPool size: " + pool.getPoolSize() + "; Steals: " + pool.getStealCount());
+        // NOTE: This test neglects stripping the additional effort needed for auto-boxing from 'long' to 'Long'
+        return result;
     }
 
-    private static List<Integer> initializeList(int l) {
-        final ArrayList<Integer> longs = new ArrayList<Integer>();
-        for (int i = 1; i <= l; i++) {
+    /**
+     * Creates an {@link ArrayList} containing all integers from 1 to the specified value.
+     * @param lastListEntry - positive integer which shall be taken as the last/ highest entry of the returned list
+     * @return list containing integers from 1 to #lastListEntry
+     */
+    private static List<Integer> initializeList(int lastListEntry) {
+        final ArrayList<Integer> longs = new ArrayList<>();
+        for (int i = 1; i <= lastListEntry; i++) {
             longs.add(i);
         }
         return longs;
+    }
+
+    private static void doTest(int iterations, int sumUpTo) {
+        System.out.println("Starting test! Summarize numbers from 1 to " + sumUpTo + " for " + iterations + " times!");
+        final List<Integer> list = initializeList(sumUpTo);
+
+        Long result = (long) 0;
+        long start = System.nanoTime();
+        for(long m = 0; m< iterations; m++) {
+            // do it sequentially
+            long res = 0;
+            for (long n : list) {
+                // sumOddEntries odd numbers
+                res += (n % 2 == 0) ? 0 : n;
+            }
+            result = res;
+        }
+        long elapsedTime = ((System.nanoTime() - start) / NANOS_PER_MS);
+        System.out.println("Result (seq):      " + result + "; Duration: " + elapsedTime
+                + "ms; Duration/iter: " + (elapsedTime / iterations) + "ms");
+
+
+        // do it sequentially with static utility method
+        start = System.nanoTime();
+        for(long n = 0; n< iterations; n++) {
+            result = ForkJoinSummarizer.sumOddEntries(list);
+        }
+        elapsedTime = ((System.nanoTime() - start) / NANOS_PER_MS);
+        System.out.println("Result (seq static): " + result + "; Duration: " + elapsedTime
+                + "ms; Duration/iter: " + (elapsedTime / iterations) + "ms");
+
+
+        // (potentially) do it in parallel
+        final ForkJoinPool pool = new ForkJoinPool();
+        start = System.nanoTime();
+        for(long n = 0; n< iterations; n++) {
+            final ForkJoinSummarizer summary = new ForkJoinSummarizer(list, SEQ_THRESHOLD);
+
+            // actually 'Runtime.getRuntime().availableProcessors()' is used by default!
+            result = pool.invoke(summary);
+        }
+        elapsedTime = ((System.nanoTime() - start) / NANOS_PER_MS);
+        System.out.println("Result (parallel): " + result + "; Duration: " + elapsedTime
+                + "ms; Duration/iter: " + (elapsedTime / iterations) + "ms; Parallelism: " + pool.getParallelism()
+                + "; ThreadPool size: " + pool.getPoolSize()+ "; Avg. Steal/iter: " + (pool.getStealCount() / iterations));
+    }
+
+
+    /**
+     * Threshold indicating when sequential work is more efficient.
+     */
+    private static final int SEQ_THRESHOLD = 800;
+    private static final long NANOS_PER_MS = 1000000L;
+
+    public static void main(String[] args) {
+        System.out.println("Warming up...");
+        doTest(1_000, 2_000_000);
+        System.out.println("Warm up finished.");
+
+        System.out.println("\nStart measuring...");
+        doTest(10_000, 2_000_000);
+        System.out.println("Measuring done.");
+
     }
 }
